faece019db746b8c96bc90c8fbf94e3e00e9acd9
Alexandr Chabatul <sashkill94@gmail.com>
2025-04-10 22:10:28 +0300

    temporary swap

--- fairy-tail/src/main.jsx
+++ fairy-tail/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import LizaHealingStory from './LizaHealingStory.jsx'
 import App from './App.jsx'
+import LizaHappy from './LizaHappy.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LizaHappy />
   </StrictMode>,
 )
