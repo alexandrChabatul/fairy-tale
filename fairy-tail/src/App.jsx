2edcb34357f8c15146dd2155d93bf0eabd36f69f
Alexandr Chabatul <sashkill94@gmail.com>
2025-04-10 22:04:36 +0300

    правки

--- fairy-tail/src/App.jsx
+++ fairy-tail/src/App.jsx
@@ -9,6 +9,18 @@ function App() {
 
   return (
     <Router>
+      <div style={{ padding: '20px', textAlign: 'center', alignSelf: "flex-start" }}>
+        <Link to="/liza-healing">
+          <button style={{ margin: '0 10px', padding: '10px 20px' }}>
+            Liza Healing
+          </button>
+        </Link>
+        <Link to="/liza-happy">
+          <button style={{ margin: '0 10px', padding: '10px 20px' }}>
+            Liza Happy
+          </button>
+        </Link>
+      </div>
       <Routes>
         <Route path="/liza-healing" element={<LizaHealingStory />} />
         <Route path="/liza-happy" element={<LizaHappy />} />
